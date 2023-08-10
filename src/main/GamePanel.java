b72dd9ffa8bacce626615f6574c7e6c4908ba06b
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-08-10 13:10:41 -0500

    added key inputs and mouse inputs

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,18 +1,50 @@
 package main;
 
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel {
 
+    private MouseInputs mouseInputs;
+    private int xDelta = 100, yDelta = 100;
+
     public GamePanel(){
 
+        // initializing mouse inputs
+        mouseInputs = new MouseInputs(this);
+
+        // adds key listener to panel
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+    }
+
+    // Method to change xDelta and yDelta for movement
+    public void changeXDelta(int value) {
+        this.xDelta += value;
+        repaint();
+    }
+
+    public void changeYDelta(int value) {
+        this.yDelta += value;
+        repaint();
+    }
+
+    public void setRectPos(int x, int y){
+        this.xDelta = x;
+        this.yDelta = y;
+        repaint();
     }
 
     // Method to draw graphics on to panel
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
-        g.fillRect(100, 100, 200, 50);
+        g.fillRect(xDelta, yDelta, 200, 50);
     }
 }
