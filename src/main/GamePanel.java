14911380a818730552b9d9d743828c56c416182b
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-08-11 18:45:04 -0500

    working on fps

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -13,6 +13,12 @@ public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
     private int xDelta = 100, yDelta = 100;
 
+    private int xDir = 1, yDir = 1;
+
+    private int frames = 0;
+
+    private long lastCheck = 0;
+
     public GamePanel(){
 
         // initializing mouse inputs
@@ -27,24 +33,44 @@ public class GamePanel extends JPanel {
     // Method to change xDelta and yDelta for movement
     public void changeXDelta(int value) {
         this.xDelta += value;
-        repaint();
     }
 
     public void changeYDelta(int value) {
         this.yDelta += value;
-        repaint();
     }
 
     public void setRectPos(int x, int y){
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
     }
 
     // Method to draw graphics on to panel
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
+        updateRectangle();
+
         g.fillRect(xDelta, yDelta, 200, 50);
+
+        frames++;
+        if(System.currentTimeMillis() - lastCheck >= 1000){
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS: " + frames);
+            frames = 0;
+        }
+
+        repaint();
+    }
+
+    private void updateRectangle() {
+        xDelta += xDir;
+        if(xDelta > 400 || xDelta < 0){
+            xDir *= -1;
+        }
+
+        yDelta += yDir;
+        if(yDelta > 400 || yDelta < 0){
+            yDir *= -1;
+        }
     }
 }
